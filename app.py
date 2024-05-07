c389b75dd769b4e975271838b5c100646c2eaf96
ahmadch-25 <83905076+ahmadch-25@users.noreply.github.com>
2024-05-07 23:59:56 +0500

    app changes

--- app.py
+++ app.py
@@ -1,9 +1,39 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request
 from flask import render_template
+import tensorflow_hub as hub
+from flask_sqlalchemy import SQLAlchemy
+import random
+import tensorflow as tf
+loaded_model = tf.saved_model.load("trainedmodel")
+
+# db = SQLAlchemy()
 
 app = Flask(__name__)
 
+# app.config['SECRET_KEY'] = random.randint(32)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+# db.init_app(app)
+
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template("index.html")
+
+
+@app.route("/predict", methods=['GET', 'POST'])
+def predict():
+    form_data = request.form
+    essay = form_data.get('essay')
+    new_essay = [essay]
+
+    # predicted_grade = model.predict([new_essay])
+    # print(f"The predicted grade for the new essay is: {predicted_grade[0]}")
+    outpit = loaded_model(new_essay)
+
+    print(f"The predicted grade for the new essay is: {outpit[0]}")
+    print(essay)
+    return render_template("index.html", score=8)
+
+
+if __name__=='__main__':
+    app.run(debug=True)
